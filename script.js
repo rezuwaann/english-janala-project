63910ce5e9b28bb6580bcb9b83a5d16bb5fa1274
Rezuwaann <ahmedrezuwan20@gmail.com>
2025-09-09 02:14:13 +0600

    Added modal

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const displayLessons = (lessons) => {
         const btnDiv = document.createElement('div')
 
         btnDiv.innerHTML = `
-        <button href="" class="btn btn-soft btn-primary" onclick="loadLevelWord(${lesson.level_no})"><i class=" fa-solid
+        <button id="lesson-btn-${lesson.level_no}" class="btn btn-soft btn-primary" onclick="loadLevelWord(${lesson.level_no})"><i class=" fa-solid
                             fa-book-open"></i>Lesson -${lesson.level_no}</button>
 
         `
@@ -23,43 +23,97 @@ const displayLessons = (lessons) => {
 }
 loadLessons()
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll('.active');
+    lessonButtons.forEach(button => button.classList.remove('active')
+
+    )
+}
+
+const loadWordDetail = async(id) => {
+
+    const url=`https://openapi.programming-hero.com/api/word/${id}`;
+    const res =await fetch(url);
+    const details=await res.json();   
+    displayWordDetails(details.data)
+}
+
+const displayWordDetails=(word)=>{
+    const detailsContainer=document.getElementById('details-container')
+    detailsContainer.innerHTML=
+      `   <div>
+                    <h1 class="font-semibold text-4xl">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h1>
+                </div>
+
+                <div>
+                    <h1 class="mb-2 font-semibold text-2xl">Meaning</h1>
+                    <h2 class="hind-shiliguri font-medium text-2xl">${word.meaning}</h2>
+                </div>
+
+                <div>
+                    <h1 class="mb-2 font-semibold text-2xl">Example</h1>
+                    <h2 class="hind-shiliguri text-2xl">${word.sentence}</h2>
+                </div>
+
+                <div>
+                    <h2 class="mb-2 hind-shiliguri font-medium text-2xl">সমার্থক শব্দ গুলো</h2>
+                    <button class="btn bg-[#1A91FF10]">Enthusiastic</button>
+                    <button class="btn bg-[#1A91FF10]">excited</button>
+                    <button class="btn bg-[#1A91FF10]">keen</button>
+                </div>`
+
+    document.getElementById('my_modal_5').showModal();
+}
+
+
 const loadLevelWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(json => displayLevelWord(json.data))
+        .then((json) => {
+            removeActive(); //function for remove all active class
+            const clickBtn = document.getElementById(`lesson-btn-${id}`)
+
+            clickBtn.classList.add("active"); //adds active class
+
+            displayLevelWord(json.data);
+        }
+        )
 }
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
 
-if (words.length===0) {
-    
-   wordContainer.innerHTML=`
+    if (words.length === 0) {
+
+        wordContainer.innerHTML = `
    <div class="text-center col-span-full hind-shiliguri roundeed py-10 space-y-3">
 <img src="./assets/alert-error.png" alt="" class="mx-auto">
             <h2 class="text-[14px] text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</h2>
             <h1 class=" text-3xl font-medium">নেক্সট Lesson এ যান</h1>
         </div>
    `
-}
+    }
 
     words.forEach(word => {
-        console.log(word)
+
         const wordCard = document.createElement('div');
-        wordCard.innerHTML= `
+        wordCard.innerHTML = `
         <div class="bg-white text-center rounded-xl shadow-sm py-10 px-5 space-y-4 h-full">
-            <h2 class="font-bold text-2xl">${word.word?word.word:"শব্দ পাওয়া যায়নি"}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <h3 class="font-medium text-lg ">Meaning /Pronounciation</h3>
-            <h2 class="hind-shiliguri font-semibold text-2xl text-[#18181B]">"${word.meaning?word.meaning:"অর্থ পাওয়া যায়নি"} / ${word.pronunciation?word.pronunciation:"উচ্চারণ পাওয়া যায়নি"}"</h2>
+            <h2 class="hind-shiliguri font-semibold text-2xl text-[#18181B]">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</h2>
             <div class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" onclick="loadWordDetail(${word.id})"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"> <i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `
         wordContainer.append(wordCard)
 
+        // my_modal_5.showModal()
+
     })
 }
+
